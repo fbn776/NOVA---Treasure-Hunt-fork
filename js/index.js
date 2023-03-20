40d2783a64178a58f9976b53d9070e135fb68c29
fbn776 <fbn776@gmail.com>
2023-03-20 22:31:44 +0530

    Restructured files

---
+++ js/index.js
@@ -0,0 +1,50 @@
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
+const qImgElement = document.getElementById('qpic')
+
+
+let state = {}
+
+function startGame() {
+  state = {}
+  showTextNode(0)
+}
+
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+  textElement.innerText = textNode.text;
+  var ritem = qImgElement.childNodes[0];
+  var img = document.createElement('img');
+  let img_dir = "images/";
+  img.src = img_dir.concat(textNode.image);
+  img.width = 300;
+  qImgElement.replaceChild(img, ritem);
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+  }
+
+  textNode.options.forEach(option => {
+    if (showOption(option)) {
+      const button = document.createElement('button')
+      button.innerText = option.text
+      button.classList.add('btn')
+      button.addEventListener('click', () => selectOption(option))
+      optionButtonsElement.appendChild(button)
+    }
+  })
+}
+
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state)
+}
+
+function selectOption(option) {
+  const nextTextNodeId = option.nextText
+  if (nextTextNodeId <= 0) {
+    return startGame()
+  }
+  state = Object.assign(state, option.setState)
+  showTextNode(nextTextNodeId)
+}
+
+startGame()
